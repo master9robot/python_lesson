3f0785af94edfb60e951587eaa0b2ada32289151
master9robot <elijahiloba9@gmail.com>
2021-10-17 15:17:54 +0100

    to add more code

--- lesson.py
+++ lesson.py
@@ -1,4 +1,9 @@
 from datetime import datetime
 
 print(datetime.now())
-# makeing a change to for test
+# makeing a change to for testnew
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
