de01db9d6d0654d57ef5254ddc9960a580189532
master9robot <elijahiloba9@gmail.com>
2021-10-17 16:07:39 +0100

    to Learn

---
+++ data types.py
@@ -0,0 +1,59 @@
+
+x = "Hello World"	#str
+x = 20	#int
+x = 20.5 #float
+x = 1j	#complex
+x = ["apple", "banana", "cherry"]	#list
+x = ("apple", "banana", "cherry")	#tuple
+x = range(6)	#range
+x = {"name" : "John", "age" : 36}	#dict
+x = {"apple", "banana", "cherry"}	#set
+x = frozenset({"apple", "banana", "cherry"})	#frozenset
+x = True	#bool
+x = b"Hello"	#bytes
+x = bytearray(5)	#bytearray
+x = memoryview(bytes(5)) #memoryview
+
+
+a = "ELIJAH"
+b = 10
+c = 10.4
+d = 10j
+e = ["pokemon", "dragons", "robots"]
+f = ("pokemon", "dragons", "robots")
+g = range(10)
+h = {"name": "elijah", "age": "10"}
+i = {"pokemon", "dragons", "robots"}
+j = frozenset({"pokemon", "dragons", "robots"})
+k = True
+L = b"ELIJAH"
+m = bytearray(10)
+n = memoryview(bytes(10))
+a
+type(a)
+b
+type(b)
+c
+type(c)
+d
+type(d)
+type(e)
+e
+f
+type(f)
+g
+type(g)
+h
+type(h)
+i
+type(i)
+j
+type(j)
+k
+type(k)
+L
+type(L)
+m
+type(m)
+n
+type(n)
